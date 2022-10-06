87e45ac2b7392b74f44c9ef9ff8e52b2b6cf48d7
胡华鑫 <544112666@qq.com>
2022-10-06 08:36:25 +0800

    端口修改

--- xueyi-ui/vue.config.js
+++ xueyi-ui/vue.config.js
@@ -30,12 +30,12 @@ module.exports = {
   // webpack-dev-server 相关配置
   devServer: {
     host: 'localhost',
-    port: 8081,
+    port: 80,
     open: true,
     proxy: {
       // detail: https://cli.vuejs.org/config/#devserver-proxy
       [process.env.VUE_APP_BASE_API]: {
-        target: `http://localhost:8081`,
+        target: `http://localhost:80`,
         changeOrigin: true,
         pathRewrite: {
           ['^' + process.env.VUE_APP_BASE_API]: ''
